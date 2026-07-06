dded0f1928ff166804e00fadbe2c19a1bacf4b14
deepanshuuiiitd <deepanshu24178@iiitd.ac.in>
2026-07-06 18:55:37 +0530

    Sync LeetCode solution: Build Array from Permutation (C++)

---
+++ Easy/2048_Build_Array_from_Permutation.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem: Build Array from Permutation
+ * Problem ID: 2048
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 20.3 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-06
+ */
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> result(nums.size());
+        for(int i =0; i < nums.size(); i++){
+            result[i] = nums[nums[i]];
+        }
+        return result;
+        
+    }
+};
