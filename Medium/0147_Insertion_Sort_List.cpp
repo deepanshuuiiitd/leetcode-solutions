04ab315b4d6df98c01dd27a4ca9d766eb9aac702
deepanshuuiiitd <deepanshu24178@iiitd.ac.in>
2026-07-09 23:44:01 +0530

    Sync LeetCode solution: Insertion Sort List (C++)

---
+++ Medium/0147_Insertion_Sort_List.cpp
@@ -0,0 +1,46 @@
+/*
+ * Problem: Insertion Sort List
+ * Problem ID: 147
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 25 ms
+ * Memory: 14.6 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-09
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* next = curr->next;
+
+            ListNode* prev = dummy;
+
+            while (prev->next && prev->next->val < curr->val) {
+                prev = prev->next;
+            }
+
+            curr->next = prev->next;
+            prev->next = curr;
+
+            curr = next;
+        }
+
+        return dummy->next;
+    }
+};
