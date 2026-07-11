1b9550c48944c3d0bfa97368fa444e6408c4d335
deepanshuuiiitd <deepanshu24178@iiitd.ac.in>
2026-07-11 23:28:12 +0530

    Sync LeetCode solution: Intersection of Two Linked Lists (C++)

--- Easy/0160_Intersection_of_Two_Linked_Lists.cpp
+++ Easy/0160_Intersection_of_Two_Linked_Lists.cpp
@@ -3,8 +3,8 @@
  * Problem ID: 160
  * Difficulty: Easy
  * Language: C++
- * Runtime: 56 ms
- * Memory: 23.8 MB
+ * Runtime: 58 ms
+ * Memory: 24 MB
  * Synced From: LeetCode
  * Date: 2026-07-11
  */
@@ -17,49 +17,72 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* slow = headA ;
-        ListNode* fast = headB;
-        int count1 = 0;
-        int count2 = 0;
 
-        while (slow){
-            count1++;
-            slow = slow ->next;
-        }
-        while (fast){
-            count2++;
-            fast = fast ->next;
-        }
-        slow = headA;
-        fast = headB;
-        int count3;
-            if (count2 > count1){
-                count3 = count2 -count1;
-                while (count3){
-                    fast = fast-> next;
-                    count3--;
-                }
-            }
-            if (count2 < count1){
-                count3 = count1 -count2;
-                while (count3){
-                    slow = slow-> next;
-                    count3--;
-                }
-            }
-        while (slow && fast){
-            
-            if (slow == fast){
-                return slow;
-                break;
-            }
-            slow = slow-> next;
-            fast = fast-> next;
-        }
-        return NULL;
+    ListNode* pA = headA;
+    ListNode* pB = headB;
 
+    while (pA != pB) {
+
+        if (pA == NULL)
+            pA = headB;
+        else
+            pA = pA->next;
+
+        if (pB == NULL)
+            pB = headA;
+        else
+            pB = pB->next;
     }
+
+    return pA;
+}
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode* slow = headA ;
+//         ListNode* fast = headB;
+//         int count1 = 0;
+//         int count2 = 0;
+
+//         while (slow){
+//             count1++;
+//             slow = slow ->next;
+//         }
+//         while (fast){
+//             count2++;
+//             fast = fast ->next;
+//         }
+//         slow = headA;
+//         fast = headB;
+//         int count3;
+//             if (count2 > count1){
+//                 count3 = count2 -count1;
+//                 while (count3){
+//                     fast = fast-> next;
+//                     count3--;
+//                 }
+//             }
+//             if (count2 < count1){
+//                 count3 = count1 -count2;
+//                 while (count3){
+//                     slow = slow-> next;
+//                     count3--;
+//                 }
+//             }
+//         while (slow && fast){
+            
+//             if (slow == fast){
+//                 return slow;
+//                 break;
+//             }
+//             slow = slow-> next;
+//             fast = fast-> next;
+//         }
+//         return NULL;
+
+//     }
 };
