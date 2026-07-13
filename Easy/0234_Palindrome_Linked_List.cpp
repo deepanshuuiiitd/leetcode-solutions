2c9cfdc921d9e51d1ac3b866c1842146a5236899
deepanshuuiiitd <deepanshu24178@iiitd.ac.in>
2026-07-13 23:59:23 +0530

    Sync LeetCode solution: Palindrome Linked List (C++)

---
+++ Easy/0234_Palindrome_Linked_List.cpp
@@ -0,0 +1,60 @@
+/*
+ * Problem: Palindrome Linked List
+ * Problem ID: 234
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 4 ms
+ * Memory: 118.1 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-13
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+
+        if (!head || !head->next)
+            return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* prev = nullptr;
+        ListNode* curr = slow->next;
+
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            if (first->val != second->val)
+                return false;
+
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
