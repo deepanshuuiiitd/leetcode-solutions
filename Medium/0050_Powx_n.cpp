231b15b774367979fdb3f8d5b1f421860352db0f
deepanshuuiiitd <deepanshu24178@iiitd.ac.in>
2026-07-20 01:40:17 +0530

    Sync LeetCode solution: Pow(x, n) (C++)

---
+++ Medium/0050_Powx_n.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem: Pow(x, n)
+ * Problem ID: 50
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 8.6 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-19
+ */
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power = n;
+
+        // Handle negative power
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double ans = 1.0;
+
+        while (power > 0) {
+            // If power is odd
+            if (power % 2 == 1) {
+                ans *= x;
+            }
+
+            x *= x;
+            power /= 2;
+        }
+
+        return ans;
+    }
+};
