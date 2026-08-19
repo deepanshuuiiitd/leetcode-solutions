1e164d2eb6e4bdb6ed336339165e13beb810f5cb
deepanshuuiiitd <deepanshu24178@iiitd.ac.in>
2026-08-19 22:23:29 +0530

    Sync LeetCode solution: Koko Eating Bananas (C++)

--- Medium/0907_Koko_Eating_Bananas.cpp
+++ Medium/0907_Koko_Eating_Bananas.cpp
@@ -3,7 +3,7 @@
  * Problem ID: 907
  * Difficulty: Medium
  * Language: C++
- * Runtime: 15 ms
+ * Runtime: 0 ms
  * Memory: 22.9 MB
  * Synced From: LeetCode
  * Date: 2026-08-19
@@ -11,47 +11,38 @@
 
 class Solution {
 public:
-
-    bool canEat(vector<int>& piles,
-                int h,
-                int k)
-    {
+    bool is_possible(int k,int h,vector<int>& piles){
         long long hours = 0;
-
-        for (int pile : piles) {
-            hours += ceil((double)pile / k);
+        for (int i: piles){
+            if(i % k == 0){
+                hours += i/k;
+            }
+            else{
+                hours += i/k +1;
+            }
         }
 
-        return hours <= h;
+        return hours<=h;
+
     }
+    int minEatingSpeed(vector<int>& piles, int h) {
+       int high = *max_element(piles.begin(),piles.end());
+       int n = piles.size();
+       int ans = 0;
 
-    int minEatingSpeed(vector<int>& piles,
-                       int h)
-    {
-        int low = 1;
+       int low = 1;
+       
+       
+       while(low<high){
+            int mid = (low+high)/2;
 
-        int high =
-            *max_element(piles.begin(),
-                         piles.end());
-
-        int ans = high;
-
-        while (low <= high)
-        {
-            int mid =
-                low + (high - low) / 2;
-
-            if (canEat(piles, h, mid))
-            {
-                ans = mid;
-                high = mid - 1;
+            if(is_possible(mid,h,piles)){
+                high = mid;
             }
-            else
-            {
-                low = mid + 1;
+            else{
+                low = mid+1;
             }
-        }
-
-        return ans;
+       }
+       return high;
     }
 };
